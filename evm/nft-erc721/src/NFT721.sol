ae71206eb2a44c36ea6934182531270d845f27b4
Samyak Narnaware <110155540+Sammmmx@users.noreply.github.com>
2026-04-26 21:30:59 +0530

    Update NFT721.sol

--- evm/nft-erc721/src/NFT721.sol
+++ evm/nft-erc721/src/NFT721.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.27;
+pragma solidity 0.8.26;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
